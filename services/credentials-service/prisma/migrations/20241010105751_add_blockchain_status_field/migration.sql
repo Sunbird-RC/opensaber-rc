08d46f1a845eaf03debce7373bfe5770f25c06b5
ashwin275 <110539449+ashwin275@users.noreply.github.com>
2025-01-23 13:17:23 +0530

    feat(anchor-to-cord): Credentials-Services -set default null for blockchain status instead of pending

--- services/credentials-service/prisma/migrations/20241010105751_add_blockchain_status_field/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "BlockchainStatusEnum" AS ENUM ('PENDING', 'ANCHORED', 'FAILED');
-
--- AlterTable
-ALTER TABLE "VerifiableCredentials" ADD COLUMN     "blockchainStatus" "BlockchainStatusEnum" NOT NULL DEFAULT 'PENDING';
